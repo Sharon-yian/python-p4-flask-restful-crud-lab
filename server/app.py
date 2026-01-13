cb6831f147cec65401380ddfe254329224e7714c
Sharon Soyian Lenjir <sharon.lenjir@student.moringaschool.com>
2026-01-13 04:29:09 +0300

    restful-crud

--- server/app.py
+++ server/app.py
@@ -1,55 +1,59 @@
-#!/usr/bin/env python3
-
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, jsonify, request
 from flask_migrate import Migrate
-from flask_restful import Api, Resource
-
 from models import db, Plant
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
 
-migrate = Migrate(app, db)
 db.init_app(app)
+migrate = Migrate(app, db)
 
-api = Api(app)
+@app.route('/plants', methods=['GET'])
+def get_plants():
+    plants = Plant.query.all()
+    return jsonify([plant.to_dict() for plant in plants])
 
+@app.route('/plants/<int:id>', methods=['GET'])
+def get_plant(id):
+    plant = Plant.query.get_or_404(id)
+    return jsonify(plant.to_dict())
 
-class Plants(Resource):
+@app.route('/plants', methods=['POST'])
+def create_plant():
+    data = request.get_json()
+    plant = Plant(
+        name=data['name'],
+        image=data['image'],
+        price=data['price'],
+        is_in_stock=data.get('is_in_stock', True)
+    )
+    db.session.add(plant)
+    db.session.commit()
+    return jsonify(plant.to_dict()), 201
 
-    def get(self):
-        plants = [plant.to_dict() for plant in Plant.query.all()]
-        return make_response(jsonify(plants), 200)
+@app.route('/plants/<int:id>', methods=['PATCH'])
+def update_plant(id):
+    plant = Plant.query.get_or_404(id)
+    data = request.get_json()
+    
+    for key, value in data.items():
+        if hasattr(plant, key):
+            setattr(plant, key, value)
+    
+    db.session.commit()
+    return jsonify(plant.to_dict())
 
-    def post(self):
-        data = request.get_json()
+@app.route('/plants/<int:id>', methods=['DELETE'])
+def delete_plant(id):
+    plant = Plant.query.get_or_404(id)
+    db.session.delete(plant)
+    db.session.commit()
+    return '', 204
 
-        new_plant = Plant(
-            name=data['name'],
-            image=data['image'],
-            price=data['price'],
-        )
-
-        db.session.add(new_plant)
-        db.session.commit()
-
-        return make_response(new_plant.to_dict(), 201)
-
-
-api.add_resource(Plants, '/plants')
-
-
-class PlantByID(Resource):
-
-    def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
-
-
-api.add_resource(PlantByID, '/plants/<int:id>')
-
+@app.route('/')
+def home():
+    return 'Plant Store API'
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/debug.py
+++ server/debug.py
@@ -4,6 +4,5 @@ from app import app
 from models import db, Plant
 
 if __name__ == '__main__':
-    
     with app.app_context():
-        import ipdb; ipdb.set_trace()
+        pass
